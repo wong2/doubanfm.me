ec1d4f333018ec8f7de7ec6e47430e81068ec4b9
wong2 <wonderfuly@gmail.com>
2013-03-27 17:29:28 +0800

    add bookmarklet.js

---
+++ bookmarklet/bookmarklet.js
@@ -0,0 +1,6 @@
+!function(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://doubanfm.me/crawl.js';
+    document.body.appendChild(script);
+}();
