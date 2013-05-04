8fb1be33cd3604873aba82d44640e915670775b4
wong2 <wonderfuly@gmail.com>
2013-05-04 12:31:20 +0800

    add config

---
+++ config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    douban: {
+        api_key: '0db50f91724f1a95291f2eb456ed4fbf',
+        cookie: ''
+    },
+    mongo: {
+        server: 'localhost:27017/doubanfm'
+    }
+};
