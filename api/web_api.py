e3e2fb7851edaf5ddb73dd28cd9bc10668d1fc9e
wong2 <wonderfuly@gmail.com>
2013-08-27 10:39:41 +0800

    add lyric api

--- api/web_api.py
+++ api/web_api.py
@@ -1,7 +1,8 @@
 #-*-coding:utf-8-*-
 
 from album import DoubanAlbum
-from flask import Flask
+from flask import Flask, request, make_response
+import requests
 import json
 
 from gevent import monkey
@@ -21,6 +22,20 @@ def get_album_info(album_id):
         'songs': album.get_songs(),
         'cover_image': album.get_cover_image_url()
     })
+
+@app.route('/lyric')
+def get_lyric():
+    sid = request.args.get('sid')
+    ssid = request.args.get('ssid')
+    url = 'http://api.douban.com/v2/fm/lyric?sid=%s&ssid=%s' % (sid, ssid)
+    r = requests.get(url)
+    json_result = r.json()
+    data = json.dumps({
+        'lyric': json_result.get('lyric')
+    })
+    response = make_response(data)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
